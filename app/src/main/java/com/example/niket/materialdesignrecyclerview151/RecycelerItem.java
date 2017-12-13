b4308c24e734cb50d59232eeab9598ff1b458162
Niket Goel <niket03@gmail.com>
2017-12-13 22:41:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/niket/materialdesignrecyclerview151/RecycelerItem.java
@@ -0,0 +1,21 @@
+package com.example.niket.materialdesignrecyclerview151;
+
+/**
+ * Created by niket on 7/2/2017.
+ */
+
+public class RecycelerItem {
+    private String name;
+
+    public RecycelerItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
